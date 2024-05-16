f8e7b7fef102a1921b99500d05af4c9f7b95d650
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-05-16 15:24:22 +0200

    creata la tabella, il button e levento estrazione random

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-/* 1. creare una tabella da 76 celle, numerate da 1 a 76 */
+const mainAllTable = document.getElementById("allTabel");
+const sectionTable = document.getElementById("tabel");
+const sectionButton = document.getElementById("sectionButton");
 
+/* 1. creare una tabella da 76 celle, numerate da 1 a 76 */
+for (let index = 0; index <= 75; index++) {
+  const cellaTabel = document.createElement("div");
+  cellaTabel.classList.add("celleTabel");
+  const numeroTabel = document.createElement("h3");
+  cellaTabel.appendChild(numeroTabel);
+  sectionTable.appendChild(cellaTabel);
+  numeroTabel.innerText = index + 1;
+  console.log(cellaTabel);
+  console.log(numeroTabel);
+  console.log(index);
+}
 /* 2. crea un bottone per l'estrazione, il numero dovra essere random da 1 a 76 */
+
+/* creazione del button */
+const buttonForExtraction = document.createElement("button");
+buttonForExtraction.innerText = "Estrai un numero";
+sectionButton.appendChild(buttonForExtraction);
+
+buttonForExtraction.addEventListener("click", function (event) {
+  Math.floor(Math.random() * 76) + 1;
+  console.log(Math.floor(Math.random() * 76) + 1);
+});
+/* function randomExtraction(max) {
+  });
+ */
 
 /* 3. dopo il clik del bottone evidenzia la cella corrispondente al numero cambiando il coloro o il bordo*/
 
