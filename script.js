dc4ed48b7219bc1452b291f0cb0f5710455e65a5
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-05-16 13:55:54 +0200

    inizio esercizi

---
+++ script.js
@@ -0,0 +1,7 @@
+/* 1. creare una tabella da 76 celle, numerate da 1 a 76 */
+
+/* 2. crea un bottone per l'estrazione, il numero dovra essere random da 1 a 76 */
+
+/* 3. dopo il clik del bottone evidenzia la cella corrispondente al numero cambiando il coloro o il bordo*/
+
+/* 4. mantieni evidenziato le celle che sono state estratte in precedenza */
